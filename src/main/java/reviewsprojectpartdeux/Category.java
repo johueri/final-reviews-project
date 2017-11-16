8d24c521e2c2a9a6acb4164462ce3c92f861772a
Jonathan Ohueri <jonathan.ohueri@gmail.com>
2017-11-16 15:25:20 -0500

    Let's get started. Cloned old project.

---
+++ src/main/java/reviewsprojectpartdeux/Category.java
@@ -0,0 +1,45 @@
+package reviewsprojectpartdeux;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue
+	private Long categoryId;
+	
+	private String categoryTitle;
+	
+	@OneToMany (mappedBy = "category")
+	private Set<Review> reviewInCategory;
+	
+	protected Category() {
+	}
+
+	public Category(String categoryTitle) {
+		this.categoryTitle = categoryTitle;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+
+	public String getCategoryTitle() {
+		return categoryTitle;
+	}
+
+	public Set<Review> getReviewInCategory() {
+		return reviewInCategory;
+	}
+	@Override
+	public String toString() {
+		return String.format("Category[categoryTitle='%s']", categoryTitle);
+}
+}
